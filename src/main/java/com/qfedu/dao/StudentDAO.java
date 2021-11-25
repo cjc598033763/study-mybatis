cff5ac059b0ec7ba5c282d6aeb96c0f0dd37b0be
cjc <dj123456>
2021-11-26 02:16:33 +0800

    创建查询 等

--- src/main/java/com/qfedu/dao/StudentDAO.java
+++ src/main/java/com/qfedu/dao/StudentDAO.java
@@ -1,9 +1,20 @@
 package com.qfedu.dao;
 
 import com.qfedu.pojo.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+// 在MyBatis 进行条件查询操作
+// 1.如果操作方法只有一个简单类型或者字符串类型的参数，在Mapper配置中可以直接通过#{key}直接获取
+// 2.如果操作方法有一个对象类型的参数，在Mapper配置中可以直接通过#{attrName}获取对象的指定属性值（attName 必须是参数对象的属性）
+// 3，如果操作方法有一个Map类型的参数，在Mapper配置中可以直接通过${key} 获得key对应的value
 
 public interface StudentDAO {
     public int insertStudent(Student student);
     public int deleteStudent(String stuNum);
     public int updateStudent(Student student);
+    public List<Student> listStudents();
+    public Student queryStudent(String stuNum);
+    public List<Student> listStudentsByPage(@Param("start")int start, @Param("pageSize")int PageSize);
+    public int getCount();
 }
--- src/test/java/qfedu/dao/StudentDAOTest.java
+++ src/test/java/qfedu/dao/StudentDAOTest.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -25,7 +26,7 @@ public class StudentDAOTest {
         // 通过会话获取DAO对象
         StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
         // 测试StudentDAO中的方法
-        int i = studentDAO.insertStudent(new Student(3,"10004","张三","男的",1));
+        int i = studentDAO.insertStudent(new Student(9,"10009","张三","男的",1));
         // 需要手动提交
         sqlSession.commit();
         System.out.println(i);
@@ -66,12 +67,105 @@ public class StudentDAOTest {
             SqlSession sqlSession = factory.openSession();
             // 通过SqlSession 通过调用getMapper方法获取DAO接口对象
             StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
-            int i = studentDAO.updateStudent(new Student(0,"10004","张三","男的",1));
+            int i = studentDAO.updateStudent(new Student(2,"10006","张三","男的",1));
             sqlSession.commit();
             System.out.println(i);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    @org.junit.Test
+    public void testSelectStudent(){
+        try {
+            InputStream is = null;
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            //
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            // SqlSessionFactory 表示MyBatis的会话工厂
+            SqlSessionFactory factory = builder.build(is);
+            // SqlSession 表示MyBatis 与 数据库之间的会话，通过工厂方法设计模式
+            SqlSession sqlSession = factory.openSession();
+            // 通过SqlSession 通过调用getMapper方法获取DAO接口对象
+            StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
+            List<Student> student = studentDAO.listStudents();
+            for (Student s: student
+                 ) {
+                System.out.println(s.toString());
+
+            }
+            sqlSession.commit();
+            System.out.println(student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @org.junit.Test
+    public void testSelectStudent2(){
+        try {
+            InputStream is = null;
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            //
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            // SqlSessionFactory 表示MyBatis的会话工厂
+            SqlSessionFactory factory = builder.build(is);
+            // SqlSession 表示MyBatis 与 数据库之间的会话，通过工厂方法设计模式
+            SqlSession sqlSession = factory.openSession();
+            // 通过SqlSession 通过调用getMapper方法获取DAO接口对象
+            StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
+            Student student = studentDAO.queryStudent("10004");
+            sqlSession.commit();
+            System.out.println(student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @org.junit.Test
+    public void testSelectStudentByQuery(){
+        try {
+            InputStream is = null;
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            //
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            // SqlSessionFactory 表示MyBatis的会话工厂
+            SqlSessionFactory factory = builder.build(is);
+            // SqlSession 表示MyBatis 与 数据库之间的会话，通过工厂方法设计模式
+            SqlSession sqlSession = factory.openSession();
+            // 通过SqlSession 通过调用getMapper方法获取DAO接口对象
+            StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
+            List<Student> student = studentDAO.listStudentsByPage(0,1);
+            for (Student s: student
+            ) {
+                System.out.println(s.toString());
+
+            }
+            sqlSession.commit();
+            System.out.println(student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @org.junit.Test
+    public void testSelectStudentByCount(){
+        try {
+            InputStream is = null;
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            //
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            // SqlSessionFactory 表示MyBatis的会话工厂
+            SqlSessionFactory factory = builder.build(is);
+            // SqlSession 表示MyBatis 与 数据库之间的会话，通过工厂方法设计模式
+            SqlSession sqlSession = factory.openSession();
+            // 通过SqlSession 通过调用getMapper方法获取DAO接口对象
+            StudentDAO studentDAO = sqlSession.getMapper(StudentDAO.class);
+            int i  = studentDAO.getCount();
+            System.out.println(i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     }
